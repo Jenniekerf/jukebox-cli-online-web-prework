056a2619c7aee6ad89d9ccaaa9934368038fa7a2
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-07 21:36:10 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -24,9 +24,9 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
-  my_songs.each {|key, value|
+  my_songs.each do |key, value|
   puts #{key}
-  }
+  end
 end
 
 
