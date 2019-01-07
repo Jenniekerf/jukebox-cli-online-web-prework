010e35c9baca967bd3ef9e9498e5978fe5cb3182
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-07 21:25:46 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -29,6 +29,13 @@ end
 
 
 def play(my_songs)
+  
+  puts "Please enter a song name:"
+  response = gets.chomp
+  if my_songs.keys.include?(response)
+    system "open #filename}"
+  else 
+    puts ""
   #this method is slightly different!
   #you should still ask the user for input and collect their song choice
   #this time, only allow user's to input a song name
