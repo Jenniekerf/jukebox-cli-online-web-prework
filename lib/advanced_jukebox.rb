f2e7c9448a53e34ebf420f1b5d4f29d3692515c3
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-07 21:19:28 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -13,10 +13,12 @@
 # }
 
 def help
-  
-
-end
-
+ puts "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can displays
+  - play : lets you choose a song to play
+  - exit : exits this program"
+end 
 
 
 def list(my_songs)
